ad6be0f814ee3e10b8e5b3aec738e4bed9e3ded0
Arthur Briganti Gini <arthurbriganti@hotmail.com>
2020-07-01 23:43:19 -0300

    hashtag tweet crawler

---
+++ Automacao/Web/Tweet_crawler.py
@@ -0,0 +1,22 @@
+import tweepy
+import csv
+import pandas as pd
+
+#senha = senha.input('Senha do twitter: ')
+
+#Entrada de credenciais 
+
+
+#Acesso a plataforma
+auth = tweepy.OAuthHandler(consumer_key, consumer_pass)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+#Abrindo e criando o arquivo
+csvfile = open('hash.csv', 'a')
+csvWriter = csv.writer(csvfile)
+
+#Criando a pesquisa
+for tweet in tweepy.Cursor(api.search, q="#RAP", count=5, lang="en", since ="2020-04-01").items():
+    print(tweet.created_at, tweet.text)
+    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
